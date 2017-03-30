a23444b2b50d96effe39882ff06e3fab4420ffb7
NitinBuzz <reachnitinvenkat@gmail.com>
2017-03-30 23:18:43 +0530

    Updated

--- src/com/nitin/MessageAPI/MessagePOSTAPI.java
+++ src/com/nitin/MessageAPI/MessagePOSTAPI.java
@@ -6,6 +6,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import jdk.nashorn.internal.runtime.Undefined;
+
 @Path("Message")
 public class MessagePOSTAPI {
 		@POST
@@ -13,9 +15,19 @@ public class MessagePOSTAPI {
 		@Produces(MediaType.TEXT_PLAIN)
 		@Path("postMessage")
 		public String getMes(String data){
-			
+			String regex = "-";
+			String[] d1 = data.split(regex);
+			String name = d1[0];
+			String name1 = "";
+			name = name.substring(1, name.length()).toUpperCase();
+			if(name.contains("UNDEFINED")){
+				name1 = "Avenger";
+			}
+			else{
+				name1 = name;
+			}
 			System.out.println(data);
-			return "Message was posted successfully";
+			return ("Thanks for the Message "+name1+", Love you...");
 		}
 
 	}
